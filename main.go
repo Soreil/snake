0d2df96f6c1effe30934cd34f7ce250db998d52c
Sjon Kosse <sjon@x61.eu>
2015-01-24 17:12:34 +0100

    Mostly working basic snake, no collision detection for the snake itself and nothing fancy

--- main.go
+++ main.go
@@ -15,18 +15,22 @@ const (
 	DOWN
 )
 
+type point struct {
+	x int
+	y int
+}
+
 type snake struct {
 	length    int
 	direction int
 }
 
 type board struct {
-	width  int
-	height int
-	snakeX int
-	snakeY int
-	appleX int
-	appleY int
+	width      int
+	height     int
+	snake      point
+	apple      point
+	snakeParts []point
 }
 
 //Create an apple at a random position
@@ -34,15 +38,16 @@ func (bPtr *board) newApple() {
 	time := time.Now()
 	rand.Seed(time.UnixNano())
 	b := bPtr
-	b.appleX = rand.Intn(b.width)
-	b.appleY = rand.Intn(b.height)
-	//TODO: change the next part to match all cells containing a snake part
-	if b.appleX == b.snakeX && b.appleY == b.snakeY {
-		b.newApple()
-	} else {
-		//draw apple
-		termbox.SetCell(b.appleX, b.appleY, '*', termbox.ColorRed, termbox.ColorDefault)
+	b.apple.x = rand.Intn(b.width)
+	b.apple.y = rand.Intn(b.height)
+	//check if the Apple will spawn inside of the snake, otherwise retry
+	for _, v := range b.snakeParts {
+		if v == b.apple {
+			b.newApple()
+		}
 	}
+	//draw apple
+	termbox.SetCell(b.apple.x, b.apple.y, ' ', termbox.ColorDefault, termbox.ColorRed)
 }
 
 func moveSnake(sPtr *snake, bPtr *board) {
@@ -50,16 +55,26 @@ func moveSnake(sPtr *snake, bPtr *board) {
 	board := bPtr
 	switch snake.direction {
 	case LEFT:
-		board.snakeX--
+		board.snake.x--
 	case UP:
-		board.snakeY--
+		board.snake.y--
 	case RIGHT:
-		board.snakeX++
+		board.snake.x++
 	case DOWN:
-		board.snakeY++
+		board.snake.y++
+	}
+	board.snakeParts = append(board.snakeParts, board.snake)
+	//do not remove the last part of the snake and increase the length
+	if board.apple == board.snake {
+		board.newApple()
+		snake.length++
+		//remove the last part of the snake
+	} else if len(board.snakeParts) > 3 {
+		termbox.SetCell(board.snakeParts[0].x, board.snakeParts[0].y, ' ', termbox.ColorDefault, termbox.ColorDefault)
+		board.snakeParts = board.snakeParts[1:]
 	}
 	//Draw Snake starting position
-	termbox.SetCell(board.snakeX, board.snakeY, '#', termbox.ColorDefault, termbox.ColorDefault)
+	termbox.SetCell(board.snake.x, board.snake.y, ' ', termbox.ColorDefault, termbox.ColorWhite)
 }
 
 func main() {
@@ -79,28 +94,67 @@ func main() {
 	//Set up the board
 	board := new(board)
 	board.width, board.height = termbox.Size()
+	board.width--
+	board.height--
 
 	//Spawn Snake
-	board.snakeX = board.width / 2
-	board.snakeY = board.height / 2
+	board.snake.x = board.width / 2
+	board.snake.y = board.height / 2
 
 	for i := 0; i < snake.length; i++ {
 		moveSnake(snake, board)
 	}
 
-	//Spawn a Apple
+	//Spawn an Apple
 	board.newApple()
 
-	for board.snakeX <= board.width && board.snakeX >= 0 && board.snakeY <= board.height && board.snakeY >= 0 {
-		break
-		/* Game loop */
+	//Collect key presses
+	event_queue := make(chan termbox.Event)
+	go func() {
+		for {
+			event_queue <- termbox.PollEvent()
+		}
+	}()
+
+	for board.snake.x <= board.width && board.snake.x >= 0 && board.snake.y <= board.height && board.snake.y >= 0 {
+		select {
+		//If an arrow key has been pressed move the direction to the arrow key
+		case event := <-event_queue:
+			switch event.Type {
+			case termbox.EventKey:
+				switch event.Key {
+				case termbox.KeyArrowDown:
+					snake.direction = DOWN
+				case termbox.KeyArrowLeft:
+					snake.direction = LEFT
+				case termbox.KeyArrowRight:
+					snake.direction = RIGHT
+				case termbox.KeyArrowUp:
+					snake.direction = UP
+				}
+			}
+			//After a frame time is done move and draw
+			//TODO: This should be changed since at the moment if a key is pressed the time resets
+		case <-time.After(time.Second / 15):
+			moveSnake(snake, board)
+			termbox.Flush()
+		}
 	}
+	termbox.SetCell(0+board.width/2, board.height/2, 'G', termbox.ColorWhite, termbox.ColorRed)
+	termbox.SetCell(1+board.width/2, board.height/2, 'A', termbox.ColorWhite, termbox.ColorRed)
+	termbox.SetCell(2+board.width/2, board.height/2, 'M', termbox.ColorWhite, termbox.ColorRed)
+	termbox.SetCell(3+board.width/2, board.height/2, 'E', termbox.ColorWhite, termbox.ColorRed)
+	termbox.SetCell(4+board.width/2, board.height/2, ' ', termbox.ColorWhite, termbox.ColorRed)
+	termbox.SetCell(5+board.width/2, board.height/2, 'O', termbox.ColorWhite, termbox.ColorRed)
+	termbox.SetCell(6+board.width/2, board.height/2, 'V', termbox.ColorWhite, termbox.ColorRed)
+	termbox.SetCell(7+board.width/2, board.height/2, 'E', termbox.ColorWhite, termbox.ColorRed)
+	termbox.SetCell(8+board.width/2, board.height/2, 'R', termbox.ColorWhite, termbox.ColorRed)
 	termbox.Flush()
 
-	//Handle input, quit when Escape is pressed.
+	//rip game
 inputLoop:
 	for {
-		event := termbox.PollEvent()
+		event := <-event_queue
 		switch event.Type {
 		case termbox.EventKey:
 			if event.Key == termbox.KeyEsc {
